2fb9a87a325e6bf9d6fa70ce912027f1f80a9fc5
Faynmen <faynmen.fan@gti.com.cn>
2023-09-21 17:28:21 +0800

    019over

---
+++ BlazorEcommerce/Server/Data/DataContext.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorEcommerce.Server.Data
+{
+	public class DataContext:DbContext
+	{
+        public DataContext(DbContextOptions<DbContext>options):base(options)
+        {
+            
+        }
+
+        public DbSet<Driver> Drivers { get; set; }
+    }
+}
